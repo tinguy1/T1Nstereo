5394b4d8d1f54ef2d77a81f4ddf823ce73c06156
tinguy1 <130340201+tinguy1@users.noreply.github.com>
2023-05-01 21:01:00 -0500

    Update T1Nstereo.plugin.js

--- T1Nstereo.plugin.js
+++ T1Nstereo.plugin.js
@@ -82,7 +82,7 @@ module.exports = (() => {
       name: 'T1Nstereo', //dont try to change the name of the plugin trust me ;)
       version: '1.0.1',
       description:
-        'stereo plugin blah blah blah blah blah. disable echo cancellation, noise reduction, noise suppression, Diagnostic audio recording, and Debug logging for this plugin to work, open plugin settings to see configurable settings with some more utilitys also im very fucking sorry that the settings panel is very messy and confusing,every time i try to make it more neat with catagorys the config wont build correctly idk why',
+        'stereo plugin blah blah blah blah blah. disable echo cancellation, noise reduction, noise suppression, Diagnostic audio recording, and Debug logging for this plugin to work, open plugin settings to see configurable settings.',
       authors: [
         {
           name: 'tinguy1'
