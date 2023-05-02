1d2127145db1ffeeeaa22a0838ff1fd91d53ae5a
tinguy1 <130340201+tinguy1@users.noreply.github.com>
2023-05-02 17:43:41 -0500

    Update T1Nstereo.plugin.js

--- T1Nstereo.plugin.js
+++ T1Nstereo.plugin.js
@@ -478,8 +478,7 @@ module.exports = (() => {
                 voiceModule.prototype,
                 'updateVideoQuality',
                 this.replacement.bind(this)
-              )    
-              console.log(voiceModule.prototype)             
+              )        
             } else {
               process.abort(0)
             }
